0fb40319001c1ea53505b7d3dd6bf25018640faf
Junyeong Heo <hhjjyy1122@gmail.com>
2026-01-06 12:38:25 +0900

    1.9: 연습문제 1-2

--- browser.py
+++ browser.py
@@ -3,64 +3,85 @@ import ssl
 
 
 class URL:
+    schemes = ["http", "https", "file"]
+
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https", "file"]
+        assert self.scheme in self.schemes
 
-        if self.scheme == "http":
-            self.port = 80
-        elif self.scheme == "https":
-            self.port = 443
+        match self.scheme:
+            case "http" | "https":
+                if "/" not in url:
+                    url = url + "/"
+                self.host, url = url.split("/", 1)
+                self.path = "/" + url
 
-        if "/" not in url:
-            url = url + "/"
-        self.host, url = url.split("/", 1)
-        self.path = "/" + url
+                if self.scheme == "http":
+                    self.port = 80
+                if self.scheme == "https":
+                    self.port = 443
 
-        if ":" in self.host:
-            self.host, port = self.host.split(":", 1)
-            self.port = int(port)
+                if ":" in self.host:
+                    self.host, port = self.host.split(":", 1)
+                    self.port = int(port)
+
+            case "file":
+                self.host = None
+                self.path = url
+                self.port = None
 
     def request(self, headers={}):
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-            proto=socket.IPPROTO_TCP,
-        )
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
+        match self.scheme:
+            case "http" | "https":
+                s = socket.socket(
+                    family=socket.AF_INET,
+                    type=socket.SOCK_STREAM,
+                    proto=socket.IPPROTO_TCP,
+                )
+                if self.scheme == "https":
+                    ctx = ssl.create_default_context()
+                    s = ctx.wrap_socket(s, server_hostname=self.host)
 
-        address = (self.host, self.port)
-        s.connect(address)
+                address = (self.host, self.port)
+                s.connect(address)
 
-        request = f"GET {self.path} HTTP/1.1\r\n"
-        request += f"Host: {self.host}\r\n"
-        request += "Connection: close\r\n"
-        for key, value in headers.items():
-            request += f"{key}: {value}\r\n"
-        request += "\r\n"
-        s.send(request.encode("utf8"))
+                request = f"GET {self.path} HTTP/1.1\r\n"
+                request += f"Host: {self.host}\r\n"
+                request += "Connection: close\r\n"
+                for key, value in headers.items():
+                    request += f"{key}: {value}\r\n"
+                request += "\r\n"
+                s.send(request.encode("utf8"))
 
-        response = s.makefile("r", encoding="utf8", newline="\r\n")
-        statusline = response.readline()
-        version, status, explanation = statusline.split(" ", 2)
+                response = s.makefile("r", encoding="utf8", newline="\r\n")
+                statusline = response.readline()
+                version, status, explanation = statusline.split(" ", 2)
 
-        response_headers = {}
-        while True:
-            line = response.readline()
-            if line == "\r\n":
-                break
-            header, value = line.split(":", 1)
-            response_headers[header.casefold()] = value.strip()
+                response_headers = {}
+                while True:
+                    line = response.readline()
+                    if line == "\r\n":
+                        break
+                    header, value = line.split(":", 1)
+                    response_headers[header.casefold()] = value.strip()
 
-        assert "transfer-encoding" not in response_headers
-        assert "content-encoding" not in response_headers
+                # assert "transfer-encoding" not in response_headers
+                assert "content-encoding" not in response_headers
 
-        body = response.read()
-        s.close()
+                body = response.read()
+                s.close()
 
-        return body
+                return body
+
+            case "file":
+                try:
+                    with open(self.path) as f:
+                        body = f.read()
+                        return body
+                except FileNotFoundError:
+                    with open("./src/index.html") as f:
+                        body = f.read()
+                        return body
 
 
 def show(body):
